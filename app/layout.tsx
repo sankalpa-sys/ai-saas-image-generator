80fe687503d06b0bf7260d512a2eb08a76ed7488
sankalpaneupane <sneupane@hamropatro.com>
2024-12-14 17:06:28 +0545

    fix: remove publishable key from client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
 }>) {
     return (
         <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
             appearance={{
                 variables: {
                     colorPrimary: "#624cf5"
